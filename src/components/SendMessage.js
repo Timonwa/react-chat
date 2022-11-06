6ae3e95c4707713d3f7f1b751debe72a445e2a0a
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-11-06 21:28:20 +0100

    update..

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -21,7 +21,6 @@ const SendMessage = ({ scroll }) => {
     });
     setMessage("");
     scroll.current.scrollIntoView({ behavior: "smooth" });
-    scroll.current.scrollIntoView(true);
   };
   return (
     <form onSubmit={(event) => sendMessage(event)} className="send-message">
