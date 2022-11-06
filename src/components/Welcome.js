dcc21d2ef05b3df422f587003ac631ed957391ef
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2022-11-06 12:03:18 +0100

    added react logo to welcome apge.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -12,6 +12,7 @@ const Welcome = () => {
   return (
     <main className="welcome">
       <h2>Welcome to React Chat.</h2>
+      <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} />
       <p>Sign in with Google to chat with with your fellow React Developers.</p>
       <button className="sign-in">
         <img
