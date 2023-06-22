1a1b75ed92c534b44a6c60ac21cdebd7d00d6ca1
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-06-22 15:22:31 +0100

    updated sininwithredirect to signinwithpopup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,14 @@ import React from "react";
 import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const NavBar = () => {
   const [user] = useAuthState(auth);
 
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
-    signInWithRedirect(auth, provider);
+    signInWithPopup(auth, provider);
   };
 
   const signOut = () => {
